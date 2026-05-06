e7ab1b05afd0b840ea55ca79f46b3cf67bc02e75
BeastDD <adongydong@gmail.com>
2026-05-06 15:45:50 -0700

    fix: clean minimal NextAuth setup + real X login button (final)

--- auth.ts
+++ auth.ts
@@ -4,29 +4,8 @@ import Twitter from 'next-auth/providers/twitter'
 export const { handlers, auth, signIn, signOut } = NextAuth({
   providers: [
     Twitter({
-      clientId: process.env.AUTH_TWITTER_ID,
-      clientSecret: process.env.AUTH_TWITTER_SECRET,
-      authorization: {
-        params: {
-          scope: 'users.read tweet.read offline.access',
-        },
-      },
+      clientId: process.env.AUTH_TWITTER_ID!,
+      clientSecret: process.env.AUTH_TWITTER_SECRET!,
     }),
   ],
-  callbacks: {
-    jwt({ token, account, profile }) {
-      if (account) {
-        token.accessToken = account.access_token
-        token.providerAccountId = profile?.id
-      }
-      return token
-    },
-    session({ session, token }) {
-      if (session.user) {
-        session.user.id = token.providerAccountId as string
-        session.accessToken = token.accessToken as string
-      }
-      return session
-    },
-  },
 })
