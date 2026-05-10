9468bd2a5fb18eadcf5635e1e6796fe7912a77ef
BeastDD <107630825+BeastDD@users.noreply.github.com>
2026-05-10 00:29:09 -0700

    Fix NextAuth v4 Twitter login button - silent fail in Edge

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,17 @@
-﻿import { handlers } from '@/auth'
+import NextAuth from "next-auth";
+import TwitterProvider from "next-auth/providers/twitter";
 
-export const { GET, POST } = handlers
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [
+    TwitterProvider({
+      clientId: process.env.AUTH_TWITTER_ID,
+      clientSecret: process.env.AUTH_TWITTER_SECRET,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async session({ session, token }) {
+      return session;
+    },
+  },
+});
